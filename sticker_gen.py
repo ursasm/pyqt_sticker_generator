05614e387e0e9286ce85f230b93046570d084302
ursasm <semen.9056@mail.ru>
2022-06-01 21:12:47 +0300

    for pyinstaller

--- sticker_gen.py
+++ sticker_gen.py
@@ -1,9 +1,22 @@
+import os
 import random
+import sys
 
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import cm, mm
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 
 def split_text(serial_numbers: str):
@@ -23,12 +36,6 @@ def generate_sn(prefix: str, b: int, e: int, str_length: int):
     if dl < len(str(e)):
         dl = len(str(e))
 
-    # res = []
-    #
-    # for i in range(b, e + 1):
-    #     res.append(f'{prefix}{i}')
-    #
-    # return res
     return [f'{prefix}{i:{dl}}'.replace(' ', '0') for i in range(b, e + 1)]
 
 
@@ -37,7 +44,7 @@ def generate_stickers(model_name: str, sn_list: list):
         return
     pdf_file_3 = f'{model_name}_{len(sn_list)}_{random.randrange(1, 10000)}.pdf'
 
-    pdfmetrics.registerFont(TTFont('Arial', 'ArialMT.ttf'))
+    pdfmetrics.registerFont(TTFont('Arial', resource_path('ArialMT.ttf')))
 
     can = canvas.Canvas(pdf_file_3)
     can.setPageSize((6 * cm, 4 * cm))
@@ -46,8 +53,8 @@ def generate_stickers(model_name: str, sn_list: list):
         can.setFont('Arial', 8)
         can.drawString(25, 85, f"Модель: {model_name}")
         can.drawString(41, 61, f"S/N: {sn}")
-        can.drawImage('LTV.jpg', 20, 16, 22.24 * mm, 10.25 * mm)
-        can.drawImage('EAC.jpg', 117, 16, 11.69 * mm, 9.13 * mm)
+        can.drawImage(resource_path('LTV.jpg'), 20, 16, 22.24 * mm, 10.25 * mm)
+        can.drawImage(resource_path('EAC.jpg'), 117, 16, 11.69 * mm, 9.13 * mm)
         can.showPage()
 
     can.save()
